d3bfbf2fbe1f6a7399cb2ed401d5850fe6f16cc2
NlVu310 <vubeo310@gmail.com>
2024-08-12 15:11:45 +0700

    commit createRQ model

--- src/main/java/com/openbanking/model/system_configuration_source/CreateSourceRQ.java
+++ src/main/java/com/openbanking/model/system_configuration_source/CreateSourceRQ.java
@@ -3,14 +3,10 @@ package com.openbanking.model.system_configuration_source;
 import com.openbanking.comon.BaseDTO;
 import lombok.Data;
 
-import javax.persistence.Column;
 @Data
-
-public class SystemConfigurationSource extends BaseDTO {
+public class CreateSourceRQ extends BaseDTO {
     private String code;
     private String info;
     private String description;
     private String status;
-    private String partnerName;
-
 }
--- src/main/java/com/openbanking/model/system_configuration_source/CreateSystemConfigurationSource.java
+++ src/main/java/com/openbanking/model/system_configuration_source/CreateSystemConfigurationSource.java
@@ -8,5 +8,5 @@ import java.util.List;
 @Data
 public class CreateSystemConfigurationSource extends BaseDTO {
     private Long partnerId;
-    private List<SystemConfigurationSource> systemConfigurationSources;
+    private List<CreateSourceRQ> systemConfigurationSources;
 }
